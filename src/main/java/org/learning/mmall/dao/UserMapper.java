3a9ab8c85ab240c006b7c6f3663d5e7c087ad425
DavidHSiang <ls5129469@163.com>
2020-05-16 18:58:39 +0800

    project init commit

---
+++ src/main/java/org/learning/mmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package org.learning.mmall.dao;
+
+import org.learning.mmall.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
